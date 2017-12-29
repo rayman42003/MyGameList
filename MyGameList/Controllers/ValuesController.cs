328938eb9f06ed18db833837cb17ad1c7f13d2ab
Richmond Chang <changrichmond@gmail.com>
2017-12-28 19:51:42 -0800

    Trying logging with Serilog

--- MyGameList/Controllers/ValuesController.cs
+++ MyGameList/Controllers/ValuesController.cs
@@ -1,36 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 
 namespace MyGameList.Controllers {
 
     [Route("api/[controller]")]
     public class ValuesController : Controller {
+        private ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger) {
+            this._logger = logger;
+        }
 
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get() {
+        public IEnumerable<string> GetAll() {
+            _logger.LogDebug("GetAll called");
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id) {
+            _logger.LogDebug($"Get {id} called");
             return "value";
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value) {
+            _logger.LogDebug($"Post {value} called");
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value) {
+            _logger.LogDebug($"Put id: {id} value: {value} called");
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id) {
+            _logger.LogDebug($"Delete {id} called");
         }
     }
 }
--- MyGameList/Program.cs
+++ MyGameList/Program.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace MyGameList {
 
     public class Program {
 
         public static void Main(string[] args) {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .Build();
     }
 }
